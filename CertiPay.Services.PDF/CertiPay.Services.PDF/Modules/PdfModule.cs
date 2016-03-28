2bdfcade1acf69199d17f4a75c29023954ccbdf2
Kyle Jones <kylebjones09@gmail.com>
2016-03-28 15:45:04 -0400

    Initial test to verify service will start and accept a route

---
+++ CertiPay.Services.PDF/CertiPay.Services.PDF/Modules/PdfModule.cs
@@ -0,0 +1,20 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertiPay.Services.PDF.Modules
+{
+    public class PdfModule : NancyModule
+    {
+        public PdfModule()
+        {
+            Get["/Pdf/GenerateDocument", runAsync: true] = async (p, ctx) =>
+            {
+                return 1;
+            };
+        }
+    }
+}
