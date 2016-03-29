6bcbe0c767c580a3d87df762cafe5dcec9074ed8
Kyle Jones <kylebjones09@gmail.com>
2016-03-29 11:54:50 -0400

    Added new check for Valid license and return valid file response

--- CertiPay.Services.PDF/Modules/PdfModule.cs
+++ CertiPay.Services.PDF/Modules/PdfModule.cs
@@ -1,6 +1,11 @@
 ﻿using CertiPay.PDF;
+using CertiPay.Services.PDF.Extensions;
 using Nancy;
+using Nancy.Responses;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using WebSupergoo.ABCpdf10;
 
 namespace CertiPay.Services.PDF.Modules
 {
@@ -10,6 +15,8 @@ namespace CertiPay.Services.PDF.Modules
         {
             Get["/Pdf/GenerateDocument"] = p =>
             {
+                if (!XSettings.InstallLicense(ConfigurationManager.AppSettings["ABCPDF-License"])) return Response.AsError(HttpStatusCode.NotFound, "ABCPDF License is Invalid");
+
                 var url = this.Request.Query["url"];
 
                 var settings = new PDFService.Settings()
@@ -20,7 +27,12 @@ namespace CertiPay.Services.PDF.Modules
                     }
                 };
 
-                return pdfSvc.CreatePdf(settings);
+                var stream = new MemoryStream(pdfSvc.CreatePdf(settings));
+
+                //Future change.  Add ability for FileName to be passed in from Caller.
+                var response = new StreamResponse(() => stream, MimeTypes.GetMimeType("Generated-Document.pdf"));
+                
+                return response;
             };
         }
     }
