9b0ef786d18b4f2ba037c2900cef30b80d80ab46
Kyle Jones <kylebjones09@gmail.com>
2016-03-29 11:04:52 -0400

    Adding Bootstrapper to set up injection

---
+++ CertiPay.Services.PDF/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy.TinyIoc;
+using CertiPay.PDF;
+
+namespace CertiPay.Services.PDF
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
+        {
+            container.Register<IPDFService, PDFService>();
+        }
+    }
+}
