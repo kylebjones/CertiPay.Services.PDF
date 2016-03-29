05ea6fc8ef20855c7d6d4430fe4bece142793a5a
Matt Wagner <mattgwagner@gmail.com>
2016-03-29 15:47:54 -0400

    Add root endpoint to return version and environment

--- CertiPay.Services.PDF/Modules/PdfModule.cs
+++ CertiPay.Services.PDF/Modules/PdfModule.cs
@@ -1,7 +1,9 @@
-﻿using CertiPay.PDF;
+﻿using CertiPay.Common;
+using CertiPay.PDF;
 using CertiPay.Services.PDF.Extensions;
 using Nancy;
 using Nancy.Responses;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
@@ -11,8 +13,21 @@ namespace CertiPay.Services.PDF.Modules
 {
     public class PdfModule : NancyModule
     {
+        private static String AppName { get { return ConfigurationManager.AppSettings["ApplicationName"]; } }
+
         public PdfModule(IPDFService pdfSvc)
         {
+            Get["/"] = _ =>
+            {
+                return Response.AsJson(new
+                {
+                    Application = AppName,
+                    Version = CertiPay.Common.Utilities.Version<PdfModule>(),
+                    Environment = EnvUtil.Current.DisplayName(),
+                    Server = Environment.MachineName
+                });
+            };
+
             Get["/Pdf/GenerateDocument"] = p =>
             {
                 if (!XSettings.InstallLicense(ConfigurationManager.AppSettings["ABCPDF-License"])) return Response.AsError(HttpStatusCode.NotFound, "ABCPDF License is Invalid");
@@ -31,7 +46,7 @@ namespace CertiPay.Services.PDF.Modules
 
                 //Future change.  Add ability for FileName to be passed in from Caller.
                 var response = new StreamResponse(() => stream, MimeTypes.GetMimeType("Generated-Document.pdf"));
-                
+
                 return response;
             };
         }
