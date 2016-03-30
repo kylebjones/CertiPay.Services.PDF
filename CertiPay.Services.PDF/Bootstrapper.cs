c9468cb0bcd3841d602b4a7b7bbf42176d730fcd
Matt Wagner <mattgwagner@gmail.com>
2016-03-30 15:40:43 -0400

    Check the environment variable for the license key

--- CertiPay.Services.PDF/Bootstrapper.cs
+++ CertiPay.Services.PDF/Bootstrapper.cs
@@ -10,7 +10,19 @@ namespace CertiPay.Services.PDF
     {
         private static String ABCPdfLicense
         {
-            get { return ConfigurationManager.AppSettings["ABCPDF-License"]; }
+            get
+            {
+                // Check the environment variables for the license key
+
+                String key = "ABCPDF-License";
+
+                if (!String.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable(key, EnvironmentVariableTarget.User)))
+                {
+                    return Environment.GetEnvironmentVariable(key, EnvironmentVariableTarget.User);
+                }
+
+                return ConfigurationManager.AppSettings[key];
+            }
         }
 
         protected override void ConfigureApplicationContainer(TinyIoCContainer container)
